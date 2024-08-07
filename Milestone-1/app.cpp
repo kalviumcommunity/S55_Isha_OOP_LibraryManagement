d9cba28e1b71025f36a2f4d80d91f0420caaaa95
IshaRode <isharode26@gmail.com>
2024-08-07 22:07:36 +0530

    Added Array of objects

--- Milestone-1/app.cpp
+++ Milestone-1/app.cpp
@@ -1,109 +1,100 @@
-#include<iostream>
-#include<conio.h>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 // Class definition for Books
 class Books {
-    private:
-        // Private data members for storing book details
-        string bookNumber, title, author, availability;
-    
-    public:
-        // Setter methods for setting book number, title, author and availability
-        void setBookNumber(string a) { this->bookNumber = a; }
-        void setTitle(string b) { this->title = b; }
-        void setAuthor(string c) { this->author = c; }
-        void setAvailability(string d) { this->availability = d; }
+private:
+    // Private data members for storing book details
+    string bookNumber, title, author, availability;
 
-        // Getter methods for book number, title, author and availability
-        string getBookNumber() { return this->bookNumber; }
-        string getTitle() { return this->title; }
-        string getAuthor() { return this->author; }
-        string getAvailability() { return this->availability; }
-        
-        // Method to display book details like book number, title, author and availability
-        void displayBook() {
-            cout << "Book Number: " << this->bookNumber << endl;
-            cout << "Title: " << this->title << endl;
-            cout << "Author: " << this->author << endl;
-            cout << "Availability: " << this->availability << endl;
-        }
+public:
+    // Setter methods for setting book number, title, author, and availability
+    void setBookNumber(string bookNumber) { this->bookNumber = bookNumber; }
+    void setTitle(string title) { this->title = title; }
+    void setAuthor(string author) { this->author = author; }
+    void setAvailability(string availability) { this->availability = availability; }
+
+    // Getter methods for book number, title, author, and availability
+    string getBookNumber() { return this->bookNumber; }
+    string getTitle() { return this->title; }
+    string getAuthor() { return this->author; }
+    string getAvailability() { return this->availability; }
+
+    // Method to display book details like book number, title, author, and availability
+    void displayBook() {
+        cout << "Book Number: " << this->bookNumber << endl;
+        cout << "Title: " << this->title << endl;
+        cout << "Author: " << this->author << endl;
+        cout << "Availability: " << this->availability << endl;
+    }
 };
 
 // Class definition for Member
 class Member {
-    private:
-        // Private data members for storing member details
-        string memberId, name, contact;
-    
-    public:
-        // Setter methods for setting member ID, name and contact
-        void setMemberId(string id) { this->memberId = id; }
-        void setName(string n) { this->name = n; }
-        void setContact(string e) { this->contact = e; }
+private:
+    // Private data members for storing member details
+    string memberId, name, contact;
 
-        // Getter methods for setting member ID, name and contact
-        string getMemberId() { return this->memberId; }
-        string getName() { return this->name; }
-        string getContact() { return this->contact; }
+public:
+    // Setter methods for setting member ID, name, and contact
+    void setMemberId(string memberId) { this->memberId = memberId; }
+    void setName(string name) { this->name = name; }
+    void setContact(string contact) { this->contact = contact; }
 
-        // Method to display member details like member ID, name and contact
-        void displayMember() {
-            cout << "Member ID: " << this->memberId << endl;
-            cout << "Name: " << this->name << endl;
-            cout << "Contact: " << this->contact << endl;
-        }
+    // Getter methods for setting member ID, name, and contact
+    string getMemberId() { return this->memberId; }
+    string getName() { return this->name; }
+    string getContact() { return this->contact; }
+
+    // Method to display member details like member ID, name, and contact
+    void displayMember() {
+        cout << "Member ID: " << this->memberId << endl;
+        cout << "Name: " << this->name << endl;
+        cout << "Contact: " << this->contact << endl;
+    }
 };
 
 int main() {
-    // Two instances of the Books class
-    Books book1, book2;
+    // Creating an array of Books objects
+    Books books[2];
 
-    // Setting details for book1
-    book1.setBookNumber("1");
-    book1.setTitle("Staying on");
-    book1.setAuthor("Paul Stock");
-    book1.setAvailability("Available");
-    
-    // Setting details for book2
-    book2.setBookNumber("2");
-    book2.setTitle("Harry Potter and the Goblet of Fire");
-    book2.setAuthor("J. K. Rowling");
-    book2.setAvailability("Available");
-    
-    // Display details for book1
-    cout << "Book 1 Details:" << endl;
-    book1.displayBook();
-    cout << endl;
+    // Setting details for the books
+    books[0].setBookNumber("1");
+    books[0].setTitle("Staying On");
+    books[0].setAuthor("Paul Scott");
+    books[0].setAvailability("Available");
 
-    // Display details for book2
-    cout << "Book 2 Details:" << endl;
-    book2.displayBook();
-    cout << endl;
+    books[1].setBookNumber("2");
+    books[1].setTitle("Harry Potter and the Goblet of Fire");
+    books[1].setAuthor("J.K. Rowling");
+    books[1].setAvailability("Available");
 
-    // Create two instances of the Member class
-    Member member1, member2;
+    // Displaying book details
+    for (int i = 0; i < 2; ++i) {
+        cout << "Book " << i + 1 << " Details:" << endl;
+        books[i].displayBook();
+        cout << endl;
+    }
 
-    // Set details for member1
-    member1.setMemberId("M1");
-    member1.setName("Isha");
-    member1.setContact("+918010992527");
+    // Creating an array of Member objects
+    Member members[2];
 
-    // Set details for member2
-    member2.setMemberId("M2");
-    member2.setName("Pranjal");
-    member2.setContact("+919699603487");
+    // Setting details for the members
+    members[0].setMemberId("M1");
+    members[0].setName("Isha");
+    members[0].setContact("+918010992527");
 
-    // Display details for member1
-    cout << "Member 1 Details:" << endl;
-    member1.displayMember();
-    cout << endl;
+    members[1].setMemberId("M2");
+    members[1].setName("Pranjal");
+    members[1].setContact("+919699603487");
 
-    // Display details for member2
-    cout << "Member 2 Details:" << endl;
-    member2.displayMember();
-    cout << endl;
+    // Displaying member details
+    for (int i = 0; i < 2; ++i) {
+        cout << "Member " << i + 1 << " Details:" << endl;
+        members[i].displayMember();
+        cout << endl;
+    }
 
     return 0;
 }
